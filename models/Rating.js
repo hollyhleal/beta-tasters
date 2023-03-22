35545dbdb1dbb27367211933a52e41b8b2629979
Michael Mount <mount.photos@gmail.com>
2023-03-22 10:26:06 -0600

    Built out Rating Model

---
+++ models/Rating.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
+
+class Rating extends Model {}
+
+Rating.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    account_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "food",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "rating",
+    timestamps: true,
+  }
+);
+
+module.exports = Rating;
