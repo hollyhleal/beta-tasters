3cf8086772d7258fee74e3fd104a7bd45f477e9d
unknown <keithgibson58@gmail.com>
2023-03-22 10:44:42 -0600

    added model an created seed for the food table

---
+++ models/Food.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Food extends Model {}
+
+Food.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Food_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Food',
+  }
+);
+
+module.exports = Food;
