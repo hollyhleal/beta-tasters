ea81221c957f280c165c5ca91e1572c2925cf75b
unknown <keithgibson58@gmail.com>
2023-03-23 14:29:26 -0600

    index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const betaRoutes = require('./betalogin-routes');
+
+const MangeRoutes = require('./Mangelogin-routes');
+
+router.use('/betalogin', betaRoutes);
+
+router.use('/Mangelogin', MangeRoutes);
+
+module.exports = router;
