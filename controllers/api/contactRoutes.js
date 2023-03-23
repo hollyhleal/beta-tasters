f32b68d89ddfcabbcb349fdf33adb4a33b0d914f
Michael Mount <mount.photos@gmail.com>
2023-03-23 14:13:00 -0600

    built contact routes and email handlebar

--- controllers/api/contactRoutes.js
+++ controllers/api/contactRoutes.js
@@ -2,7 +2,12 @@ const router = require("express").Router();
 const sendEmail = require("../../utils/emails");
 
 router.get("/", async (req, res) => {
-    try {
-        
-    }
-})
+  try {
+    res.render("contact", {});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post
