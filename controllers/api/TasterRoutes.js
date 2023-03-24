54563bd17911490b56daf82a6cec4db30df72dc4
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-03-24 12:06:46 -0600

    commented back in line 15 on tasterroutes.js

--- controllers/api/TasterRoutes.js
+++ controllers/api/TasterRoutes.js
@@ -12,8 +12,7 @@ router.post("/", async (req, res) => {
     req.session.save(() => {
       req.session.loggedIn = true;
 
-      // res.status(200).json(dbBetaData);
-      res.redirect("/menu-user");
+      res.status(200).json(dbBetaData);
     });
   } catch (err) {
     console.log(err);
