5f134a20e768a3ff965922918e05b5ddcfa997fb
Michael Mount <mount.photos@gmail.com>
2023-03-26 12:36:15 -0600

    built post route for /additem

--- controllers/api/ManagementRoute.js
+++ controllers/api/ManagementRoute.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const { Manage } = require("../../models");
+const { Manage, Food } = require("../../models");
 
+// Manager - Create Account
+// route: /api/Managelogin
 router.post("/", async (req, res) => {
   try {
     const dbManageData = await Manage.create({
@@ -10,7 +12,7 @@ router.post("/", async (req, res) => {
 
     req.session.save(() => {
       req.session.loggedInManager = true;
-
+      req.session.userId = dbManageData.id;
       res.redirect("/menu-mgr");
     });
   } catch (err) {
@@ -19,11 +21,14 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Manager Login
+
+// /api/Managelogin/login
 router.post("/login", async (req, res) => {
   try {
-    const dbManageData = await User.findOne({
+    const dbManageData = await Manage.findOne({
       where: {
-        email: req.body.email,
+        email: req.body.mgrEmail,
       },
     });
 
@@ -34,7 +39,9 @@ router.post("/login", async (req, res) => {
       return;
     }
 
-    const validPassword = await dbManageData.checkPassword(req.body.password);
+    const validPassword = await dbManageData.checkPassword(
+      req.body.mgrPassword
+    );
 
     if (!validPassword) {
       res
@@ -45,11 +52,37 @@ router.post("/login", async (req, res) => {
 
     req.session.save(() => {
       req.session.loggedInManager = true;
-
+      req.session.userId = dbManageData.id;
       res.redirect("/menu-mgr");
     });
   } catch (err) {
     console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//need to create /logout route for manager
+//http://localhost:3001/api/Managelogin/logout
+router.post("/logout", (req, res) => {
+  if (req.session.loggedInManager) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.post("/additem", async (req, res) => {
+  try {
+    const newMenuItem = await Food.create({
+      Food_name: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      management_id: req.session.userId,
+    });
+    res.status(200).json(newMenuItem);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
